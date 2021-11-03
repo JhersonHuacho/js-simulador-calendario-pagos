bbf91adfc9f3da3161d25cdbf2bad03b212b4a5f
Francisco <jhersonhuacho@gmail.com>
2021-11-02 23:05:42 -0500

    segunda entrega del proyecto final

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Las buenas practicas de código Javascript automaticamente - Instalar ESLint en 
 Author: Damian Sire Desarrollo
 https://www.youtube.com/watch?v=uUSsrs6S3d8&t=461s
 
+Eliminar todos los elementos secundarios de un nodo DOM en JavaScript
+https://qastack.mx/programming/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+
+Vanilla js onload
+https://www.bufa.es/vanilla-js-on-load/
--- js/dom.js
+++ js/dom.js
@@ -1,10 +1,38 @@
 import Credito from './credito.js';
 
+let listCalendarios = [];
+
 const form = document.querySelector('form');
 
-const generarCalendarioHtml = (calendario) => {
-  const tbody = document.querySelector('tbody');
+// Cuando el html esta cargado
+const cargarHistorialCronogramas = (paramStorageListCalendario) => {
+  if (paramStorageListCalendario !== null) {
+    const tbody = document.querySelector('.table-historial tbody');
+    tbody.innerHTML = '';
 
+    for (let index = 0; index < paramStorageListCalendario.length; index += 1) {
+      const tr = document.createElement('tr');
+
+      Object.entries(paramStorageListCalendario[index]).forEach(([key, value]) => {
+        const td = document.createElement('td');
+        td.innerText = value;
+        // console.log(key);
+        if (key === 'calendario') {
+          td.innerText = 'ver';
+          tr.appendChild(td);
+        } else {
+          tr.appendChild(td);
+        }
+      });
+      tbody.appendChild(tr);
+    }
+  }
+};
+
+// Proceso de generar calendario
+const generarCalendarioHtml = (calendario) => {
+  const tbody = document.querySelector('.table-cronograma tbody');
+  tbody.innerHTML = '';
   for (let index = 0; index < calendario.length; index += 1) {
     const tr = document.createElement('tr');
 
@@ -53,15 +81,76 @@ const generarCronograma = (paramPrestamo, paramPlazo, paramTea) => {
   }
 
   generarCalendarioHtml(calendario);
+
+  const storageCalendrio = localStorage.getItem('listCalendario');
+
+  if (storageCalendrio !== null) {
+    listCalendarios = JSON.parse(storageCalendrio);
+  }
+
+  const newSingleCalendario = {
+    id: listCalendarios.length + 1,
+    prestamo: paramPrestamo,
+    plazo: paramPlazo,
+    tea: paramTea,
+    calendario,
+  };
+
+  listCalendarios.push(newSingleCalendario);
+  localStorage.setItem('listCalendario', JSON.stringify(listCalendarios));
+
+  cargarHistorialCronogramas(listCalendarios);
 };
+
+function validarInput(paramInput, index) {
+  let validar = true;
+  const mensaje = document.querySelectorAll('.mensaje')[index];
+
+  if (paramInput.value === '') {
+    mensaje.classList.remove('none');
+    mensaje.classList.add('block');
+    validar = false;
+  } else {
+    mensaje.classList.remove('block');
+    mensaje.classList.add('none');
+  }
+
+  return validar;
+}
 
 function calcularCronograma(event) {
   event.preventDefault();
   const inputPrestamo = document.querySelector('#prestamo');
   const inputPlazo = document.querySelector('#plazo');
   const inputTea = document.querySelector('#tea');
+  let validar = true;
+
+  if (validarInput(inputPrestamo, 0) === false) {
+    validar = false;
+  }
+
+  if (validarInput(inputPlazo, 1) === false) {
+    validar = false;
+  }
+
+  if (validarInput(inputTea, 2) === false) {
+    validar = false;
+  }
+
+  if (validar === false) {
+    return;
+  }
 
   generarCronograma(inputPrestamo.value, inputPlazo.value, inputTea.value);
+
+  inputPrestamo.value = '';
+  inputPlazo.value = '';
+  inputTea.value = '';
 }
 
 form.addEventListener('submit', calcularCronograma);
+
+window.addEventListener('load', () => {
+  const storageListCalendario = localStorage.getItem('listCalendario');
+  cargarHistorialCronogramas(JSON.parse(storageListCalendario));
+});
